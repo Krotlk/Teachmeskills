636cb53f54387368018332d75c12dc7101e8c5e9
Krotlk <krotilda43@gmail.com>
2022-03-04 21:57:06 +0300

    feat: add Lesson 16

---
+++ lesson 16/index.js
@@ -0,0 +1,40 @@
+let admin;
+let firstName= "Джон";
+
+admin= firstName;
+
+alert (admin);
+
+
+let planetEarth;
+let userName;
+
+
+let x = 20;  
+let y = 58;
+let z = 42;
+
+console.log(x+y+z);
+
+
+let secondInMinutes = 60;
+let minutesInhour = 60;
+let hourInDay = 24;
+let dayInYear = 365;
+let yearInSecond=(dayInYear * hourInDay * minutesInhour * secondInMinutes);
+let myAgeInSeconds = (prompt("Введите возраст"));
+
+console.log("Year in seconds", myAgeInSeconds * yearInSecond);
+
+
+let count = 42;
+let username = "42";
+let usernameToNubmer = +username;
+let countToText = `${count}`;
+
+
+console.log(usernameToNubmer);
+console.log(countToText);
+
+
+
