683e61acde8c7680e0b6649a14df19cd84efa621
Krotlk <krotilda43@gmail.com>
2022-03-11 14:46:45 +0300

    feat: add solutions for tasks 1-5,7[HW-16]

--- lesson 16/index.js
+++ lesson 16/index.js
@@ -1,14 +1,10 @@
 let admin;
 let firstName= "Джон";
-
 admin= firstName;
 
 alert (admin);
 
-
-let planetEarth;
-let userName;
-
+//task 1
 
 let x = 20;  
 let y = 58;
@@ -16,6 +12,7 @@ let z = 42;
 
 console.log(x+y+z);
 
+//task 2
 
 let secondInMinutes = 60;
 let minutesInhour = 60;
@@ -26,15 +23,40 @@ let myAgeInSeconds = (prompt("Введите возраст"));
 
 console.log("Year in seconds", myAgeInSeconds * yearInSecond);
 
+//task 3
 
 let count = 42;
 let username = "42";
 let usernameToNubmer = +username;
 let countToText = `${count}`;
 
-
 console.log(usernameToNubmer);
 console.log(countToText);
 
+//task 4
 
+let a = 1;
+let b = 2;
+let c = "белых медведей";
 
+console.log(a+=`${b}`,"",c);
+
+//task 5
+
+let wordFirst = "доступ";
+let wordSecond = "морпех";
+let wordThird = "наледь";
+let wordFour = "попрек";
+let wordFive = "рубило";
+let wordLength = wordFirst.length + wordSecond.length + wordThird.length + wordFour.length + wordFive.length;
+
+console.log(wordLength);
+
+//task 6
+
+//task 7
+
+let userName = prompt("Enter your name");
+let userAge = prompt("Enter your age");
+
+console.log("Your name:", userName,",", "Your age:", userAge);
